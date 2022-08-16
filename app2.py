10b4c7aa0438bb8c8c47959c0a935ef5080bc1b9
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-08-17 00:44:14 +0530

    using for loops inside template

--- app2.py
+++ app2.py
@@ -1,45 +1,13 @@
 from jinja2 import Template
 
-TEMPLATE = '''<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jnanipith Award</title>
-</head>
-<body>
-    <div id="Intro">
-    <h1>Bharatiya Janta Party</h1>
-    Bharatiya Jnanpith is a literary organization based in Delhi. It also presents highest literary awards in India.
-    </div>
-    <div id="main">
-        <h1>Awardees</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>Yaer</th>
-                    <th>Awardees</th>
-                    <th>Language</th>
-                </tr>
-            </thead>
-            <tbody>
-            {% for janapith in janapith_data %}
-                <tr>
-                    <td>{{ janapith["year"] }}</td>
-                    <td>{{ janapith["awardees"] }}</td>
-                    <td>{{ janapith["language"] }}</td>
-                </tr>
-            {% endfor %}
-            </tbody>
-        </table>
-    </div>
-    
-</body>
-</html>'''
 
 janapith_data = [{"year":2021,"awardees":"Krishna Gautam","language":"Hindi"},{"year":2022,"awardees":"Ritik Goyal","language":"Oria"},{"year":2015,"awardees":"Rajat Mishra","language":"English"}]
 def main():
+    #read the template file
+    template_file = open("template.html.jinja2",'r')
+    TEMPLATE = template_file.read()
+    template_file.close() #closing the file
+
     template = Template(TEMPLATE)
     content = template.render(janapith_data=janapith_data)
     myfile = open("janapith.html",'w')

9ef749d26377a4b4b299b9da223e18b3d5bbb22d
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-08-19 01:04:51 +0530

    Flask Application

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 @app.route("/hello")
 def hello_world():
-    return """
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    <meta charset="UTF-8"/>
-                </head>
-                <body>
-                    Hello World
-                </body>
-            </html>
-    """
+    return render_template("hello_world.html")
 
 if __name__ == '__main__':
     app.debug=True

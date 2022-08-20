c7b6595ffc7c4075fbcc3ad0bb9184ff62a205fd
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-08-20 23:21:55 +0530

    Flask Application

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
-@app.route("/hello")
+@app.route("/hello", methods = ["GET","POST"])
 def hello_world():
-    return render_template("index.html")
+    return render_template("form.html")
 
 if __name__ == '__main__':
     app.debug=True
     app.run()
+
+

6bb22492cba776f9c7023a5ab9ff05beeb48dacd
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-08-22 22:28:05 +0530

    Flask Application

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 @app.route("/hello", methods = ["GET","POST"])
 def hello_world():
-    return render_template("form.html")
+    if(request.method=="GET"):
+        return render_template("form.html")
+
+    elif (request.method=="POST"):
+        username = request.form["user_name"]
+        return render_template("friend.html",display_name=username )
+
+
+
 
 if __name__ == '__main__':
     app.debug=True
     app.run()
-
-

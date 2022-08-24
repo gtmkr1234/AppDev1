8d0c8d7e516844ae375e3de50e7a7561d48247e5
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-08-24 10:48:24 +0530

    SQLAlchemy

--- experiment_sqlitedb/main.py
+++ experiment_sqlitedb/main.py
@@ -8,3 +8,23 @@ from sqlalchemy.orm import Session
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
 
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__='user'
+    user_id=Column(Integer,autoincrement = True, primary_key = True)
+    username = Column(String, unique = True)
+    email = Column(String, unique= True)
+
+class Article(Base):
+    __tablename__='article'
+    article_id = Column(Integer,autoincrement = True, primary_key = True)
+    title = Column(String)
+    comtent = Column(String)
+
+class ArticleAuthors(Base):
+    __tablename__ = 'article_authors'
+    user_id = Column(Integer,ForeignKey("user.user_id"),primary_key = True, nullable = False)
+    article_id = Column(Integer,ForeignKey("article.article_id"), primary_key = True, nullable = False)
+    

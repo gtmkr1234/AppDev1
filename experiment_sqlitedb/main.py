c2d1ed0f2fd90d3d2fa14c6b06edd587ff84ba85
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-08-24 23:58:57 +0530

    SQLAlchemy database engine

--- experiment_sqlitedb/main.py
+++ experiment_sqlitedb/main.py
@@ -1,8 +1,7 @@
 import sqlalchemy
 from sqlalchemy import create_engine
-from sqlalchemy import Table,Column,Integer,String,ForeignKey
+from sqlalchemy import Table, Column, Integer, String, ForeignKey
 from sqlalchemy import select
-
 
 from sqlalchemy.orm import Session
 from sqlalchemy.orm import declarative_base
@@ -12,20 +11,24 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__='user'
-    user_id=Column(Integer,autoincrement = True, primary_key = True)
-    username = Column(String, unique = True)
-    email = Column(String, unique= True)
+    __tablename__ = 'user'
+    user_id = Column(Integer, autoincrement=True, primary_key=True)
+    username = Column(String, unique=True)
+    email = Column(String, unique=True)
+
 
 class Article(Base):
-    __tablename__='article'
-    article_id = Column(Integer,autoincrement = True, primary_key = True)
+    __tablename__ = 'article'
+    article_id = Column(Integer, autoincrement=True, primary_key=True)
     title = Column(String)
     comtent = Column(String)
-    authors = relationship("User",secondary="article_authors")
+    authors = relationship("User", secondary="article_authors")
+
 
 class ArticleAuthors(Base):
     __tablename__ = 'article_authors'
-    user_id = Column(Integer,ForeignKey("user.user_id"),primary_key = True, nullable = False)
-    article_id = Column(Integer,ForeignKey("article.article_id"), primary_key = True, nullable = False)
-    
+    user_id = Column(Integer, ForeignKey("user.user_id"), primary_key=True, nullable=False)
+    article_id = Column(Integer, ForeignKey("article.article_id"), primary_key=True, nullable=False)
+
+
+engine = create_engine("sqlite:///.testdb.sqlite3")

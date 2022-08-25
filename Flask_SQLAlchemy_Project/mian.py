2965d8dd476e53b2f8849ed473742992e4c34c10
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-08-25 23:30:51 +0530

    SQLAlchemy database integration

--- Flask_SQLAlchemy_Project/mian.py
+++ Flask_SQLAlchemy_Project/mian.py
@@ -4,16 +4,16 @@ from flask import render_template
 from flask import request
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///./testdb.sqlite3"
 db = SQLAlchemy()
 db.init_app(app)
 app.app_context().push()
 
+
 class User(db.Model):
     __tablename__ = 'user'
-    user_id = db.Column(db.Integer,autoincrement=True,primary_key=True)
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     username = db.Column(db.String, unique=True)
     email = db.Column(db.String, unique=True)
 
@@ -28,18 +28,25 @@ class Article(db.Model):
 
 class ArticleAuthors(db.Model):
     __tablename__ = 'article_authors'
-    user_id = db.Column(db.Integer,  db.ForeignKey("user.user_id"), primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.user_id"), primary_key=True, nullable=False)
     article_id = db.Column(db.Integer, db.ForeignKey("article.article_id"), primary_key=True, nullable=False)
 
 
-@app.route("/",methods =["GET","POST"] )
+@app.route("/", methods=["GET", "POST"])
 def articles():
     articles = Article.query.all()
-    return render_template("articles.html",articles=articles)
+    return render_template("articles.html", articles=articles)
+
+
+@app.route("/articles_by/<user_name>", methods=["GET", "POST"])
+def getArticles(user_name):
+    articles = Article.query.filter(Article.authors.any(username=user_name))
+    return render_template("articles.html", articles=articles)
+
 
 if __name__ == '__main__':
     app.run(
-        host = '0.0.0.0',
+        host='0.0.0.0',
         debug=True,
         port=5000
     )

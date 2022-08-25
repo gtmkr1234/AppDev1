456e234bd05649416c7d8700f72930fbc95a3e7c
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-08-25 11:25:38 +0530

    SQLAlchemy database integration

--- Flask_SQLAlchemy_Project/mian.py
+++ Flask_SQLAlchemy_Project/mian.py
@@ -4,10 +4,9 @@ from flask import render_template
 from flask import request
 from flask_sqlalchemy import SQLAlchemy
 
-current_dir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLAlchemy_DATABASE_URI'] = "sqlite:///" + os.path.join(current_dir, "testdb.sqlite3")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///./testdb.sqlite3"
 db = SQLAlchemy()
 db.init_app(app)
 app.app_context().push()
@@ -15,21 +14,31 @@ app.app_context().push()
 class User(db.Model):
     __tablename__ = 'user'
     user_id = db.Column(db.Integer,autoincrement=True,primary_key=True)
-    username = db.column(db.String, unique=True)
-    email = db.column(db.String, unique=True)
+    username = db.Column(db.String, unique=True)
+    email = db.Column(db.String, unique=True)
 
 
 class Article(db.Model):
     __tablename__ = 'article'
-    article_id = db.column(db.Integer,autoincrement=True, primary_key=True)
-    title = db.column(db.String, unique=True)
-    content = db.column(db.String, unique=True)
+    article_id = db.Column(db.Integer,autoincrement=True, primary_key=True)
+    title = db.Column(db.String, unique=True)
+    content = db.Column(db.String, unique=True)
     authors = db.relationship("User",secondary="article_authors")
 
 
-class ArticleAuthors(db.model):
+class ArticleAuthors(db.Model):
     __tablename__ = 'article_authors'
-    article_id = db.column(db.Integer,db.ForeignKey("Article.article_id"), primary_key=True, nullable=False)
-    user_id = db.column(db.Integer,db.ForeignKey("User.user_id"), primary_key=True, nullable=False)
+    article_id = db.Column(db.Integer,db.ForeignKey("Article.article_id"), primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey("User.user_id"), primary_key=True, nullable=False)
 
 
+@app.route("/",methods =["GET","POST"] )
+def home():
+    return render_template("home.html")
+
+if __name__ == '__main__':
+    app.run(
+        host = '0.0.0.0',
+        debug=True,
+        port=5000
+    )
